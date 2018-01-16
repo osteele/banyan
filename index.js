d2da5a969caa718cdc3a087eb8d8db3f7b6582bf
Oliver Steele <steele@osteele.com>
2018-01-15 20:58:37 -0500

    Use ES6 features

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-'use strict';
+import './src/Main.css';
 
-require('./src/Main.css');
-var Elm = require('./src/Main.elm');
-var Dropbox = require('dropbox')
+import Dropbox from 'dropbox';
+import Elm from './src/Main.elm';
 
-var app = Elm.Main.embed(document.getElementById('app'));
+const app = Elm.Main.embed(document.getElementById('app'));
 app.ports.dropboxClientID.send(process.env.DROPBOX_APP_KEY);
 
 app.ports.listFiles.subscribe((accessToken, pages) => {
     pages = pages || null;
-    var dbx = new Dropbox({ accessToken });
-    var cache = localStorage['fileTree'] && JSON.parse(localStorage['fileTree']);
-    var listFiles = (fn, pages) =>
+    const dbx = new Dropbox({ accessToken });
+    let cache = localStorage['fileTree'] && JSON.parse(localStorage['fileTree']);
+    function listFiles(fn, pages) {
         fn
             .then((response) => {
                 var files = response.entries.map((entry) => {
@@ -40,6 +39,7 @@ app.ports.listFiles.subscribe((accessToken, pages) => {
                 console.error(error);
                 app.ports.fileListError.send(); // error.name, error.message
             });
+    }
     if (cache) { // && cache.accessToken === accessToken) {
         app.ports.fileList.send([Object.values(cache.entries), true]);
         listFiles(dbx.filesListFolderContinue({ cursor: cache.cursor }), pages);
@@ -50,10 +50,10 @@ app.ports.listFiles.subscribe((accessToken, pages) => {
 });
 
 app.ports.getAccountInfo.subscribe((accessToken) => {
-    var dbx = new Dropbox({ accessToken });
+    const dbx = new Dropbox({ accessToken });
     dbx.usersGetCurrentAccount()
         .then((response) => {
-            var teamName = response.team ? response.team.name : "Personal";
+            const teamName = response.team ? response.team.name : "Personal";
             app.ports.setAccountInfo.send({
                 teamName,
                 name: response.name
