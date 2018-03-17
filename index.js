ac73465eea7c01a3a162cd386f519bfcfc8789f2
Oliver Steele <steele@osteele.com>
2018-03-17 11:00:58 -0400

    Remove js ListFolder implementation

--- index.js
+++ index.js
@@ -16,49 +16,10 @@ const app = Elm.Main.embed(document.getElementById('app'), {
   files: localStorage[filesCacheKey] || null,
 });
 
-// See https://www.dropbox.com/developers/documentation/http/documentation#files-list_folder
-app.ports.listFolder.subscribe(async ([accessToken, params]) => {
-  if (!accessToken) {
-    app.ports.receiveFileListError.send('internal error: access token not set');
-    return;
-  }
-  const dbx = new Dropbox({ accessToken });
-  let query = dbx.filesListFolder({
-    path: params.path,
-    recursive: params.recursive,
-    include_deleted: params.includeDeleted,
-  });
-
-  // eslint-disable-next-line no-await-in-loop
-  while (query) {
-    let response;
-    try {
-      response = await query;
-    } catch (error) {
-      console.error('listFiles:', error);
-      app.ports.receiveFileListError.send(error.message || error.error);
-      return;
-    }
-
-    const entries = response.entries.map(entry => ({
-      tag: entry['.tag'],
-      name: entry.name,
-      path_lower: entry.path_lower,
-      path_display: entry.path_display,
-      size: entry.size || null,
-    }));
-    const hasMore = Boolean(response.has_more);
-    // initiate the query before processing these entries, in case this allows
-    // for greater parallelism
-    query = hasMore && dbx.filesListFolderContinue({ cursor: response.cursor });
-    app.ports.receiveFileList.send([entries, hasMore]);
-  }
-});
-
 app.ports.saveFilesCache.subscribe((cacheValue) => {
   const json = JSON.stringify(cacheValue);
   delete localStorage[oldFileCacheKey];
-  console.info('write cache', json.length);
+  console.info('write cache', json.length, 'bytes');
   localStorage[filesCacheKey] = json;
 });
 
--- src/ListFolder.elm
+++ src/ListFolder.elm
@@ -9,26 +9,6 @@ import Json.Decode exposing (..)
 -- Elm -> JS
 
 
-{-| listFolder parameters.
-
-Doesn't implement include_media_info, include_has_explicit_shared_members, include_mounted_folders.
-
--}
-type alias ListFolderParameters =
-    { path : String
-    , recursive : Bool
-    , includeDeleted : Bool
-    }
-
-
-{-| Request that JavaScript start initiating requests to Dropbox.
-
-See: // See <https://www.dropbox.com/developers/documentation/http/documentation#files-list_folder>
-
--}
-port listFolder : ( String, ListFolderParameters ) -> Cmd msg
-
-
 port saveFilesCache : Json.Encode.Value -> Cmd msg
 
 
