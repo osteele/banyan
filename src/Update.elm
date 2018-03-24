dde584170acdc0f17bed45b8b0cd0140d233cd08
Oliver Steele <steele@osteele.com>
2018-03-24 18:59:40 -0400

    Factor Update out from Main

---
+++ src/Update.elm
@@ -0,0 +1,155 @@
+module Update exposing (update)
+
+import AccountInfo exposing (..)
+import Dropbox
+import FilesComponent
+import Message exposing (..)
+import Model exposing (..)
+import Navigation
+import Ports exposing (..)
+import TreeMap exposing (renderFileTreeMap)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        -- initial load, with saved access token
+        InitializeAccessToken accessToken ->
+            case accessToken of
+                Just tokenString ->
+                    { model
+                      -- FIXME "You should not use this in a production app."
+                        | auth = Just <| Dropbox.authorizationFromAccessToken tokenString
+                        , status = SigningIn
+                    }
+                        ! [ getAccountInfo tokenString ]
+
+                Nothing ->
+                    model ! []
+
+        -- on OAuth callback
+        AuthResponse (Dropbox.AuthorizeOk auth) ->
+            { model | auth = Just auth.userAuth, status = SignedIn }
+                ! (case auth.userAuth |> extractAccessToken of
+                    Just token ->
+                        [ getAccountInfo token
+                        , storeAccessToken token
+                        , clearLocationHash model
+                        ]
+
+                    Nothing ->
+                        []
+                  )
+
+        AuthResponse err ->
+            { model
+                | auth = Nothing
+                , errors = toString err :: model.errors
+                , status = SignedOut
+            }
+                ! []
+
+        -- user clicks sign in. redirect to OAuth
+        SignIn ->
+            model
+                ! [ Dropbox.authorize
+                        { clientId = model.clientId
+                        , state = Nothing
+                        , requireRole = Nothing
+                        , forceReapprove = False
+                        , disableSignup = False
+                        , locale = Nothing
+                        , forceReauthentication = False
+                        }
+                        model.location
+                  ]
+
+        -- user clicks sign out
+        SignOut ->
+            clearAccountFields model
+                ! [ removeAccountInfo ()
+                  , clearLocationHash model
+                  ]
+
+        SetAccountInfo info ->
+            let
+                m =
+                    { model | accountInfo = Just info, status = SignedIn }
+            in
+                if FilesComponent.isEmpty model.files then
+                    update (restoreOrSyncFiles info) m
+                else
+                    m ! []
+
+        FilesMessage filesMsg ->
+            let
+                ( m1, cmd ) =
+                    updateFilesModel filesMsg model
+
+                m =
+                    combineErrors m1
+            in
+                case filesMsg of
+                    FilesComponent.Changed ->
+                        let
+                            ( m2, cmd2 ) =
+                                update RenderFileTreeMap m
+                        in
+                            m2 ! [ cmd, cmd2 ]
+
+                    FilesComponent.Cleared ->
+                        { m | path = "/" } ! [ cmd ]
+
+                    _ ->
+                        ( m, cmd )
+
+        RenderFileTreeMap ->
+            model
+                ! [ Model.subtree model |> renderFileTreeMap model.depth ]
+
+        -- view commands
+        DismissMessageView n ->
+            -- closing message by index instead of serial number could be
+            -- a race condition, but probably less likely than the display
+            -- updating between user intention and action
+            let
+                remove n lst =
+                    List.take n lst ++ List.drop (n + 1) lst
+            in
+                { model | errors = remove n model.errors } ! []
+
+        SetFocus p ->
+            update RenderFileTreeMap { model | path = p }
+
+        SetSortOrder ord ->
+            { model | order = ord } ! []
+
+        SetTreeDepth n ->
+            { model | depth = n } ! []
+
+
+updateFilesModel : FilesComponent.Msg -> Model -> ( Model, Cmd Msg )
+updateFilesModel msg model =
+    case model.auth of
+        Just auth ->
+            let
+                ( files, cmd ) =
+                    FilesComponent.update auth msg model.files
+            in
+                { model | files = files } ! [ Cmd.map FilesMessage cmd ]
+
+        Nothing ->
+            model ! []
+
+
+{-| Remove the hash from the browser navigation URL.
+-}
+clearLocationHash : Model -> Cmd msg
+clearLocationHash model =
+    let
+        location =
+            model.location
+    in
+        Navigation.modifyUrl <|
+            String.join ""
+                [ location.protocol, "//", location.host, location.pathname ]
