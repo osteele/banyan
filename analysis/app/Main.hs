e81ccb5bfeac61b65a9eadbaeaf848980f26c6b8
Oliver Steele <steele@osteele.com>
2018-04-01 11:11:09 -0400

    Add command-line options to analysis tool

--- analysis/app/Main.hs
+++ analysis/app/Main.hs
@@ -1,21 +1,66 @@
 module Main where
 
-import System.Environment
 import Data.List
 import Data.List.Split
+import Data.Semigroup ((<>))
+import Options.Applicative
+import System.Environment
+
 import Serialize
 
+data Options = Options
+    { output      :: Maybe String
+    , relative    :: Bool
+    , stats       :: Bool
+    , input       :: String
+    }
+
+options :: Parser Options
+options = Options
+    <$> optional (strOption
+        ( long "output"
+        <> short 'o'
+        <> metavar "FILE"
+        <> help "Write output to FILE" ))
+    <*> switch
+        ( long "relative"
+        <> short 'r'
+        <> help "Use the ..-relative encoder" )
+    <*> switch
+        ( long "stats"
+        <> short 's'
+        <> help "Display statistics" )
+    <*> argument str (metavar "FILE")
+
 main :: IO ()
-main = do
-    [filename] <- getArgs
-    s <- readFile filename
-    let relPaths = splitOn ";" $ trimnl s
-    putStrLn $ "rel paths: " ++ show relPaths
-    let absPaths = decodePaths relPaths
-    putStrLn $ "abs paths: " ++ show absPaths
-    putStrLn $ "reencoded: " ++ (show $ encodePaths absPaths)
-    putStrLn $ "reencode2: " ++ (show $ encodePathsRel absPaths)
-    -- putStrLn $ intercalate ";" reencoded
+main = run =<< execParser opts
+    where
+        opts = info (options <**> helper)
+            ( fullDesc
+            <> progDesc "Report path cache metrics"
+            <> header "analyze - report path cache metrics" )
+
+run :: Options -> IO ()
+run (Options output relative stats infile) = do
+    inString <- readFile infile >>= return . trimnl
+    let encoder = if relative then encodePathsRel else encodePaths
+    let inPaths = splitOn ";" inString
+    let absPaths = decodePaths inPaths
+    let outPaths = encoder absPaths
+    let outString = intercalate ";" outPaths
+
+    if stats then do
+        putStrLn $ "input size : " ++ (show $ length inString)
+        putStrLn $ "output size: " ++ (show $ length outString)
+    else do
+        putStrLn $ "input   : " ++ show inPaths
+        putStrLn $ "decoded : " ++ show absPaths
+        putStrLn $ "re-coded: " ++ show outPaths
+
+    case output of
+        Nothing -> do return ()
+        Just output ->
+            putStrLn $ intercalate ";" outPaths
 
 trimnl =
     reverse . dropWhile (=='\n') . reverse
